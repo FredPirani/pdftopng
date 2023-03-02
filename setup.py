520865583d7285ac490bb34539b744b50ed97fc9
Federico Pirani <federico.pirani@bidcompany.it>
2023-03-02 13:26:44 +0100

    [try] With nothing

--- setup.py
+++ setup.py
@@ -4,48 +4,6 @@ import os
 import sys
 import glob
 import shutil
-try:
-    import pybind11
-except ImportError:
-    from os.path import isfile
-    from subprocess import Popen, PIPE, STDOUT
-    from shlex import quote
-
-    def execute(cmd, params = None):
-        """Execute a shell command.
-        """
-        cmd = str(cmd)
-        if isfile(cmd) and ' ' in cmd.strip():
-            cmd = '"' + cmd.strip() + '"'
-        if params is not None:
-            cmd = cmd.strip() + ' ' + \
-                ' '.join(list(map(quote, list(map(str, params)))))
-        with Popen(
-                cmd, stdout=PIPE, stderr=STDOUT, shell=True) as _proc:
-            _stdout_b, _stderr_b = _proc.communicate()
-            _ret_code = _proc.returncode
-            _stdout = ''
-            try:
-                _stdout = _stdout_b.decode('utf-8').strip()
-            except AttributeError:  # pragma: no cover
-                pass                # pragma: no cover
-            del _stdout_b
-            _stderr = ''
-            try:
-                _stderr = _stderr_b.decode('utf-8').strip()
-            except AttributeError:
-                pass
-            del _stderr_b
-            if _ret_code != 0:
-                raise Exception(
-                    'Got return code ' + str(_ret_code) + ' with error ' + str(_stderr))
-            return _stdout
-        
-    execute('pip install pybind')
-
-    import pybind11
-    del execute, quote, Popen, PIPE, STDOUT, isfile
-    
 from setuptools.command.build_ext import build_ext
 from setuptools import setup, Extension, find_packages
 
@@ -65,131 +23,6 @@ requires = [
 dev_requires = ["Pillow>=8.2.0", "pytest>=6.2.3", "pytest-cov>=2.11.1"]
 dev_requires = dev_requires + requires
 
-poppler_dir = os.path.join(os.getcwd(), "lib", "poppler")
-build_dir = os.path.join(poppler_dir, "build")
-library_dirs = []
-libraries = []
-
-if sys.platform in ["linux", "darwin"]:
-    library_dirs.extend([build_dir])
-    libraries.extend(["poppler"])
-
-if sys.platform == "win32":
-    # https://docs.python.org/3/library/platform.html#platform.architecture
-    x = "x64" if sys.maxsize > 2 ** 32 else "x86"
-
-    poppler_dir = os.path.join(os.getcwd(), "lib", "poppler")
-    build_dir = os.path.join(poppler_dir, f"build_win_{x}")
-
-    # set VCPKG_INSTALLATION_ROOT=C:\dev\vcpkg
-    vcpkg_lib_dir = os.path.join(
-        os.environ["VCPKG_INSTALLATION_ROOT"], "installed", f"{x}-windows", "lib"
-    )
-    poppler_lib_dir = os.path.join(build_dir, "Release")
-
-    library_dirs.extend([vcpkg_lib_dir, poppler_lib_dir])
-    libraries.extend(
-        ["freetype", "fontconfig", "libpng16", "jpeg", "advapi32", "poppler"]
-    )
-
-include_dirs = [
-    poppler_dir,
-    os.path.join(poppler_dir, "fofi"),
-    os.path.join(poppler_dir, "goo"),
-    os.path.join(poppler_dir, "utils"),
-    os.path.join(poppler_dir, "poppler"),
-    build_dir,
-    os.path.join(build_dir, "utils"),
-    os.path.join(build_dir, "poppler"),
-    pybind11.get_include(),
-]
-
-ext_modules = [
-    Extension(
-        "pdftopng.pdftopng",
-        # Sort input source files to ensure bit-for-bit reproducible builds
-        # (https://github.com/pybind/python_example/pull/53)
-        sorted([os.path.join("src", "pdftopng", "pdftopng.cpp")]),
-        include_dirs=include_dirs,
-        library_dirs=library_dirs,
-        libraries=libraries,
-        language="c++",
-    ),
-]
-
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    import os
-
-    with tempfile.NamedTemporaryFile("w", suffix=".cpp", delete=False) as f:
-        f.write("int main (int argc, char **argv) { return 0; }")
-        fname = f.name
-    try:
-        compiler.compile([fname], extra_postargs=[flagname])
-    except setuptools.distutils.errors.CompileError:
-        return False
-    finally:
-        try:
-            os.remove(fname)
-        except OSError:
-            pass
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14/17] compiler flag.
-    The newer version is prefered over c++11 (when it is available).
-    """
-    flags = ["-std=c++14", "-std=c++11"]
-
-    for flag in flags:
-        if has_flag(compiler, flag):
-            return flag
-
-    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-
-    c_opts = {
-        "msvc": ["/MD", "/EHsc", "/std:c++14"],
-        "unix": ["-O3", "-Wall", "-shared", "-fPIC"],
-    }
-
-    l_opts = {
-        "msvc": [],
-        "unix": [],
-    }
-
-    if sys.platform == "darwin":
-        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
-        c_opts["unix"] += darwin_opts
-        l_opts["unix"] += darwin_opts
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        link_opts = self.l_opts.get(ct, [])
-
-        if ct == "unix":
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, "-fvisibility=hidden"):
-                opts.append("-fvisibility=hidden")
-
-        for ext in self.extensions:
-            ext.define_macros = [
-                ("VERSION_INFO", '"{}"'.format(self.distribution.get_version()))
-            ]
-            ext.extra_compile_args = opts
-            ext.extra_link_args = link_opts
-
-        build_ext.build_extensions(self)
-
 
 def setup_package():
     metadata = dict(
@@ -204,7 +37,6 @@ def setup_package():
         license=about["__license__"],
         packages=find_packages(where="src", exclude=("tests",)),
         package_dir={"": "src"},
-        ext_modules=ext_modules,
         include_package_data=True,
         install_requires=requires,
         extras_require={"dev": dev_requires},
@@ -221,7 +53,6 @@ def setup_package():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
         ],
-        cmdclass={"build_ext": BuildExt},
     )
 
     setup(**metadata)
